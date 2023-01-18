41943ab8e430735d0bb4e9b8f5f591bb78c8e341
BartSUL <95762382+BartSUL@users.noreply.github.com>
2023-01-18 20:12:50 +0100

    Creation of tree pages home, about and draw it

--- di/src/App.js
+++ di/src/App.js
@@ -1,6 +1,7 @@
 // src/App.js
 
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
---
+++ di/src/pages/Drawit.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Drawit = () => {
+    return (
+        <div>
+            <h1>Start to create!</h1>
+        </div>
+    );
+};
+
+export default Drawit;
