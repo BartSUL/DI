4021c5f681ca7e453f499d55bdc857c1ec26f4f2
BartSUL <95762382+BartSUL@users.noreply.github.com>
2023-01-18 21:13:03 +0100

    creation styles files, to correct

--- di/src/components/Header.js
+++ di/src/components/Header.js
@@ -1,23 +1,28 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <div className='navigation'>
-            <ul>
-                <NavLink to="/">
-                    <li>Home page</li>
-                </NavLink>
-                <NavLink to="/about">
-                    <li>About</li>
-                </NavLink>
-                <NavLink to="/drawit">
-                    <li>Draw it!</li>
-                </NavLink>
-            </ul>
-            
-        </div>
-    );
+  return (
+    <div className="navigation">
+      <ul>
+        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+          <li>Home page</li>
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}
+        >
+          <li>About</li>
+        </NavLink>
+        <NavLink
+          to="/drawit"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}
+        >
+          <li>Draw it!</li>
+        </NavLink>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
