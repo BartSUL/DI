10635b989edcac13ceea72b32ba62a65f8cb55ec
BartSUL <95762382+BartSUL@users.noreply.github.com>
2023-01-18 20:03:48 +0100

    Installation react router dom and sass

---
+++ di/src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="title">Bienvenue sur mon site!</div>
+    </div>
+  );
+};
+
+export default App;
