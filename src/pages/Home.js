e65ff0687136f1b29f1c1049551ed1f80869f2e1
BartSUL <95762382+BartSUL@users.noreply.github.com>
2023-01-19 17:43:55 +0100

    update sur le file

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div className="">
+      <Header />
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      
+    </div>
+    
+  );
+};
+
+export default Home;
